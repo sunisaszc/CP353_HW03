abcad425f55a6c3fccd9c1a8adbe997c986aaf67
sunisa <sunisa.szc@g.swu.ac.th>
2017-09-08 23:02:29 +0700

    finish

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,17 +1,85 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
-var getGithubProfile = function getGithubProfile() {
-  return fetch('https://api.punkapi.com/v2/beers/?beer_name=brown');
+var search = function search() {
+  var text = document.getElementById("name").value;
+  var text2 = document.getElementById("food").value;
+  if (text == "" && text2 == "") {
+    getAll();
+  } else if (text == "" && text2 != "") {
+    getBeers("food=" + text2);
+  } else if (text != "" && text2 == "") {
+    getBeers("beer_name=" + text);
+  } else {
+    getBeers("beer_name=" + text + "&food=" + text2);
+  }
 };
 
-getGithubProfile().then(function (response) {
-  return response.json();
-}).catch(function (e) {
-  return console.error('Failed while fetching github user profile');
-}).then(function (response) {
-  console.log(JSON.stringify(response));
-  document.getElementById("demo").innerHTML = JSON.stringify(response);
-});
+function createNode(element) {
+  return document.createElement(element);
+}
 
-},{}]},{},[1]);
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+var url = "https://api.punkapi.com/v2/beers/?";
+
+var getBeers = function getBeers(param) {
+  fetch("" + url + param).then(function (response) {
+    return response.json();
+  }).catch(function (e) {
+    return console.error('Failed while fetching github user profile');
+  }).then(function (response) {
+    var beers = document.getElementById("beers");
+    beers.innerHTML = "";
+    for (var i = 0; i < response.length; i++) {
+      console.log(response[i].name);
+      var span = createNode('span'),
+          div = createNode('div');
+      var header = createNode('h2'),
+          img = createNode('img');
+      header.setAttribute("style", "text-align:center");
+      header.innerHTML = "" + response[i].name;
+      img.setAttribute("src", "" + response[i].image_url);
+      span.innerHTML = "<b>Tagline :  </b>" + response[i].tagline + "<br/><b>First Brewed :  </b>" + response[i].first_brewed + "<br/><b>Description : </b>";
+      span.innerHTML += "" + response[i].description;
+      span.innerHTML += "<br/><b>Food Pairing :  </b>" + response[i].food_pairing + "<br/><br/>";
+      div.setAttribute("class", "w3-panel w3-card");
+      img.setAttribute("class", "center");
+      append(beers, div);
+      append(div, header);
+      append(div, img);
+      append(div, span);
+    }
+  });
+};
+
+var getAll = function getAll() {
+  fetch('https://api.punkapi.com/v2/beers/?').then(function (response) {
+    return response.json();
+  }).catch(function (e) {
+    return console.error('Failed while fetching github user profile');
+  }).then(function (response) {
+    var beers = document.getElementById("beers");
+    beers.innerHTML = "";
+    for (var i = 0; i < response.length; i++) {
+      console.log(response[i].name);
+      var span = createNode('span'),
+          div = createNode('div');
+      var header = createNode('h2'),
+          img = createNode('img');
+      header.setAttribute("style", "text-align:center");
+      header.innerHTML = "" + response[i].name;
+      img.setAttribute("src", "" + response[i].image_url);
+      span.innerHTML = "<b>Tagline :  </b>" + response[i].tagline + "<br/><b>First Brewed :  </b>" + response[i].first_brewed + "<br/><b>Description : </b>";
+      span.innerHTML += "" + response[i].description;
+      span.innerHTML += "<br/><b>Food Pairing :  </b>" + response[i].food_pairing + "<br/><br/>";
+      div.setAttribute("class", "w3-panel w3-card");
+      img.setAttribute("class", "center");
+      append(beers, div);
+      append(div, header);
+      append(div, img);
+      append(div, span);
+    }
+  });
+};
--- js/index.js
+++ js/index.js
@@ -1,10 +1,83 @@
-const getGithubProfile = () =>
-  fetch('https://api.punkapi.com/v2/beers/?beer_name=brown')
+var search = () =>{
+  var text = document.getElementById("name").value;
+  var text2 = document.getElementById("food").value;
+  if(text == "" && text2 == ""){
+    getAll();
+  }else if(text == "" && text2 != ""){
+    getBeers("food="+text2);
+  }else if(text != "" && text2 == ""){
+    getBeers("beer_name="+text);
+  }else{
+    getBeers("beer_name="+text+"&food="+text2);
+  }
+}
 
-  getGithubProfile()
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+
+
+var url = "https://api.punkapi.com/v2/beers/?";
+
+var getBeers = (param) =>{
+  fetch(`${url}${param}`)
   .then(response => response.json())
   .catch(e => console.error('Failed while fetching github user profile'))
   .then(function(response) {
-    console.log(JSON.stringify(response));
-    document.getElementById("demo").innerHTML = JSON.stringify(response);
+    var beers = document.getElementById("beers");
+    beers.innerHTML = "";
+    for (var i = 0 ; i < response.length ; i++) {
+      console.log(response[i].name);
+      var span = createNode('span'),
+          div = createNode('div');
+      var header = createNode('h2'),
+          img = createNode('img');
+      header.setAttribute("style","text-align:center");
+      header.innerHTML = `${response[i].name}`;
+      img.setAttribute("src",`${response[i].image_url}`);
+      span.innerHTML = `<b>Tagline :  </b>${response[i].tagline}<br/><b>First Brewed :  </b>${response[i].first_brewed}<br/><b>Description : </b>`;
+      span.innerHTML += `${response[i].description}`;
+      span.innerHTML += `<br/><b>Food Pairing :  </b>${response[i].food_pairing}<br/><br/>`;
+      div.setAttribute("class", "w3-panel w3-card")
+      img.setAttribute("class","center")
+      append(beers,div);
+      append(div, header);
+      append(div,img);
+      append(div,span);
+    }
   })
+}
+
+var getAll = () =>{
+  fetch('https://api.punkapi.com/v2/beers/?')
+  .then(response => response.json())
+  .catch(e => console.error('Failed while fetching github user profile'))
+  .then(function(response) {
+    var beers = document.getElementById("beers");
+    beers.innerHTML = "";
+    for (var i = 0 ; i < response.length ; i++) {
+      console.log(response[i].name);
+      var span = createNode('span'),
+          div = createNode('div');
+      var header = createNode('h2'),
+          img = createNode('img');
+      header.setAttribute("style","text-align:center");
+      header.innerHTML = `${response[i].name}`;
+      img.setAttribute("src",`${response[i].image_url}`);
+      span.innerHTML = `<b>Tagline :  </b>${response[i].tagline}<br/><b>First Brewed :  </b>${response[i].first_brewed}<br/><b>Description : </b>`;
+      span.innerHTML += `${response[i].description}`;
+      span.innerHTML += `<br/><b>Food Pairing :  </b>${response[i].food_pairing}<br/><br/>`;
+      div.setAttribute("class", "w3-panel w3-card")
+      img.setAttribute("class","center")
+      append(beers,div);
+      append(div, header);
+      append(div,img);
+      append(div,span);
+    }
+  })
+}
